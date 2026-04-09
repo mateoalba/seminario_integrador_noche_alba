ec07044559d21e90df3e42702e533e9462ef9aaf
msteoalba <mateoalba1234gmail.com>
2026-04-08 19:51:26 -0500

    Clase_python

---
+++ Modulo_1/Python/04_Input.py
@@ -0,0 +1,9 @@
+nombre = input("Ingrese su nombre: ")
+print("Hola", nombre)
+print(f"Hola {nombre}, bienvenido al curso de Python")
+print("Hola" + nombre)
+
+edad_str= input("Ingrese su edad: ")
+print(f"Tienes, {edad_str} años")
+edad = int(edad_str)
+print(f"El doble de tu edad es: {edad * 2} años")
